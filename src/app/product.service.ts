baef4eab5fc36156a4d4c5f00f2396915db89235
Yasser <yassirsaadi4@gmail.com>
2019-10-27 14:13:23 +0100

    Commited module 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,9 +10,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number):Observable<Album>{
-    return this._http.get(this._albumUrl).map( Response => {
-      return <Album> Response.json();
-    } )
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 
 }
